c5d007adc91463a8d2f26380a539f117cb50d782
calebphiri98 <calebphiri98@gmail.com>
2026-09-09 09:51:25 +0200

    Edited the amount to 50

--- frontend/src/pages/PayPage.jsx
+++ frontend/src/pages/PayPage.jsx
@@ -14,7 +14,7 @@ export default function PayPage() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          amount: 5000,
+          amount: 50,
           currency: "MWK",
           email: "test@example.com",
           first_name: "Test",
